b96b0c773e06086adb08b22c44e5e0f16e4a3181
Joaquin-Sanchez-Reyes <joaquin.sanchez@genstudents.org>
2024-11-26 15:44:46 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+
+nombre = input("Ingresa tu nombre: ")
+apellido = input("Ingresa tu apellido: ")
+ciudad = input("Ingresa tu ciudad: ")
+
+print(f'Hola, {nombre} {apellido}. Seleccionaste la ciudad: {ciudad}')
+
+nombre_completo = f"{nombre} {apellido}"
+
+print(f"Nombre completo: {nombre_completo} ")
